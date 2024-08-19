84c2a408f4f53b3aaa08075737075fc3dca4b6e7
timothy <tandrian24@bsd.idserve.net>
2024-08-19 23:46:00 +0700

    Initial Commit

---
+++ Mini3-PanicAttack/Mini3_PanicAttackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini3_PanicAttackApp.swift
+//  Mini3-PanicAttack
+//
+//  Created by Timothy Andrian on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini3_PanicAttackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mini3-PanicAttackUITests/Mini3_PanicAttackUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mini3_PanicAttackUITestsLaunchTests.swift
+//  Mini3-PanicAttackUITests
+//
+//  Created by Timothy Andrian on 19/08/24.
+//
+
+import XCTest
+
+final class Mini3_PanicAttackUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
